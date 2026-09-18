d8c81597399ec1b7e92f9a93aba08c3b27542db5
Lav1n1a <lavinia.camk01@gmail.com>
2026-09-18 18:45:05 -0400

    feat: Config loki

--- backend/src/observability/logger.ts
+++ backend/src/observability/logger.ts
@@ -1,5 +1,38 @@
 import pino from 'pino';
 
+const transport = pino.transport({
+  targets: [
+    {
+      // Continua mostrando os logs no terminal
+      target: 'pino/file',
+
+      options: {
+        destination: 1,
+      },
+    },
+    {
+      // Envia os mesmos logs para o Loki
+      target: 'pino-loki',
+
+      options: {
+        host:
+          process.env.LOKI_URL ??
+          'http://localhost:3100',
+
+        labels: {
+          application: 'course-platform-api',
+          environment: 'development',
+        },
+
+        batching: {
+          interval: 2,
+          maxBufferSize: 1000,
+        },
+      },
+    },
+  ],
+});
+
 export const logger = pino({
   level: process.env.LOG_LEVEL ?? 'info',
 
@@ -13,4 +46,6 @@ export const logger = pino({
     ],
     remove: true,
   },
-});
+},
+ transport,
+);
